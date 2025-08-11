a96a6d848e76de82a2fa296f06332866ca873dde
4aR1i <kopninn@list.ru>
2025-08-11 10:47:02 +0300

    add splash screen settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nrkopnin.pettracker',
   appName: 'pet-tracker-app',
-  webDir: 'dist/spa'
+  webDir: 'dist/spa',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+    },
+  },
 };
 
 export default config;
